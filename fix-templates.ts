7c755bd82e2a227e9f9e52beced59b4787c1d1aa
HuXiaodie123 <3404983822@qq.com>
2026-04-30 12:47:25 +0800

    前端修改

--- fix-templates.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-function fixTpl(file: string) {
-  let content = fs.readFileSync(file, 'utf8');
-  content = content.replace(/<template>\s*<template>/g, '<template>');
-  content = content.replace(/<\/template>\s*<\/template>/g, '</template>');
-  fs.writeFileSync(file, content);
-}
-['src/views/Intelligence.vue', 'src/views/Portrait.vue'].forEach(fixTpl);
