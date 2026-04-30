222aa4cd8937c38c7dea6ab2235d9b913ad34c18
wsnc2333 <wsnc23333@163.com>
2026-04-30 16:58:40 +0800

    hippoRAG部署完成

---
+++ need/HippoRAG/src/hipporag/embedding_model/__init__.py
@@ -0,0 +1,43 @@
+from .base import EmbeddingConfig, BaseEmbeddingModel
+
+from ..utils.logging_utils import get_logger
+
+logger = get_logger(__name__)
+
+
+def _get_embedding_model_class(embedding_model_name: str = "nvidia/NV-Embed-v2"):
+    lowered_name = embedding_model_name.lower()
+
+    if "bge-m3" in lowered_name:
+        from .BGEM3 import BGEM3EmbeddingModel
+
+        return BGEM3EmbeddingModel
+    elif "GritLM" in embedding_model_name:
+        from .GritLM import GritLMEmbeddingModel
+
+        return GritLMEmbeddingModel
+    elif "NV-Embed-v2" in embedding_model_name:
+        from .NVEmbedV2 import NVEmbedV2EmbeddingModel
+
+        return NVEmbedV2EmbeddingModel
+    elif "contriever" in embedding_model_name:
+        from .Contriever import ContrieverModel
+
+        return ContrieverModel
+    elif "text-embedding" in embedding_model_name:
+        from .OpenAI import OpenAIEmbeddingModel
+
+        return OpenAIEmbeddingModel
+    elif "cohere" in embedding_model_name:
+        from .Cohere import CohereEmbeddingModel
+
+        return CohereEmbeddingModel
+    elif embedding_model_name.startswith("Transformers/"):
+        from .Transformers import TransformersEmbeddingModel
+
+        return TransformersEmbeddingModel
+    elif embedding_model_name.startswith("VLLM/"):
+        from .VLLM import VLLMEmbeddingModel
+
+        return VLLMEmbeddingModel
+    assert False, f"Unknown embedding model name: {embedding_model_name}"
